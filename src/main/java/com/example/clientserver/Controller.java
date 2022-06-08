9491977898677d1181ac344d2000fb77768f1b6c
decorix <polevg2015@yandex.ru>
2022-06-09 02:19:09 +0300

    The first window is created

---
+++ src/main/java/com/example/clientserver/Controller.java
@@ -0,0 +1,37 @@
+package com.example.clientserver;
+
+import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button LoginInAcc;
+
+    @FXML
+    private Button Register;
+
+    @FXML
+    private TextField inputLogin;
+
+    @FXML
+    private PasswordField inputPassword;
+
+    @FXML
+    void initialize() {
+        Register.setOnAction(event ->  {
+            System.out.println("Please, register!");
+        });
+    }
+
+}
